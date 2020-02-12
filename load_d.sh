84f924db3c98091c10b2bc8a434ed8b868b0f59e
chenboshuo <chenboshuo.hi@163.com>
2020-02-13 07:49:27 +0800

    :sparkles: mount D. 挂载D盘

---
+++ load_d.sh
@@ -0,0 +1,4 @@
+# #!/bin/sh
+# reference https://unix.stackexchange.com/questions/131917/how-to-mount-the-d-disk-of-windows-in-linux-mint
+sudo mkdir -p media/data
+sudo ntfs-3g /dev/sda4 media/data
