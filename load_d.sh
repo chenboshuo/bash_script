86e7fde2ca750b88d549d7485a43563cffe17840
chenboshuo <chenboshuo.hi@163.com>
2020-02-18 16:12:44 +0800

    :bug: Fix the error of path.修复相对路径错误.

--- load_d.sh
+++ load_d.sh
@@ -1,4 +1,4 @@
 # #!/bin/sh
 # reference https://unix.stackexchange.com/questions/131917/how-to-mount-the-d-disk-of-windows-in-linux-mint
-sudo mkdir -p media/data
-sudo ntfs-3g /dev/sda4 media/data
+sudo mkdir -p /media/data
+sudo ntfs-3g /dev/sda4 /media/data
